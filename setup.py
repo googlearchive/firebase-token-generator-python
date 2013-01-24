940b06600e4109bf360610ee8237f31f9c295cd1
Greg Soltis <greg@firebase.com>
2013-01-24 10:35:42 -0800

    Fix some packaging stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='firebase-token-generator',
-    version='1.0',
+    version='1.1',
     author='Greg Soltis',
     author_email='greg@firebase.com',
     py_modules=['firebase_token_generator'],
