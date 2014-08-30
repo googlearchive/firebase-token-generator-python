7f75e069f6cda11da706ca5e5ebf57a841522cb7
Chris Raynor <chris@firebase.com>
2014-08-29 17:25:22 -0700

    requiring UID in data, adding tests and updating documentation

--- README.md
+++ README.md
@@ -33,14 +33,15 @@ this snippet of Python code:
 ```python
 from firebase_token_generator import create_token
 
-arbitrary_auth_payload = {"auth_data": "foo", "other_auth_data": "bar"}
-token = create_token("<YOUR_FIREBASE_SECRET>", arbitrary_auth_payload)
+auth_payload = {"uid": "1", "auth_data": "foo", "other_auth_data": "bar"}
+token = create_token("<YOUR_FIREBASE_SECRET>", auth_payload)
 ```
 
-The arbitrary payload object passed into `create_token()` is then available for use within your
+The payload object passed into `create_token()` is then available for use within your
 security rules via the [`auth` variable](https://www.firebase.com/docs/security/api/rule/auth.html).
 This is how you pass trusted authentication details (e.g. the client's user ID) into your
-Firebase rules.
+Firebase rules. It must contain a "uid" key, which must be a string of less than 256 chars long,
+and the generated token must be less than 1024 chars long in total.
 
 
 ## Token Options
@@ -69,7 +70,7 @@ Here is an example of how to use the second `options` argument:
 ```python
 from firebase_token_generator import create_token
 
-arbitrary_auth_payload = {"auth_data": "foo", "other_auth_data": "bar"}
+auth_payload = {"uid": "1", "auth_data": "foo", "other_auth_data": "bar"}
 options = {"admin": True}
-token = create_token("<YOUR_FIREBASE_SECRET>", arbitrary_auth_payload, options)
+token = create_token("<YOUR_FIREBASE_SECRET>", auth_payload, options)
 ```
---
+++ firebase_token_generator_test.py
@@ -0,0 +1,57 @@
+from firebase_token_generator import create_token
+import unittest
+
+class TestTokenGenerator(unittest.TestCase):
+
+    def test_smoke_test(self):
+        token = create_token("barfoo", {"uid": "foo"})
+        self.assertIsInstance(token, basestring)
+
+    def test_malformed_key(self):
+        with self.assertRaises(ValueError):
+            token = create_token(1234567890, {"uid": "foo"})
+
+    def test_no_uid(self):
+        with self.assertRaises(ValueError):
+            token = create_token("barfoo", {"blah": 5})
+
+    def test_invalid_uid(self):
+        with self.assertRaises(ValueError):
+            token = create_token("barfoo", {"uid": 5, "blah": 5})
+
+    def test_uid_max_length(self):
+        #length:                                        10        20        30        40        50        60        70        80        90       100       110       120       130       140       150       160       170       180       190       200       210       220       230       240       250   256
+        token = create_token("barfoo", {"uid": "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456"})
+        self.assertIsInstance(token, basestring)
+
+    def test_uid_too_long(self):
+        with self.assertRaises(ValueError):
+            #length:                                        10        20        30        40        50        60        70        80        90       100       110       120       130       140       150       160       170       180       190       200       210       220       230       240       250    257
+            token = create_token("barfoo", {"uid": "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567"})
+
+    def test_uid_min_length(self):
+        token = create_token("barfoo", {"uid": ""})
+        self.assertIsInstance(token, basestring)
+
+    def test_token_too_long(self):
+        with self.assertRaises(RuntimeError):
+            create_token("barfoo", {"uid": "blah", "long_var": "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345612345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234561234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456"})
+
+    def test_no_uid_with_admin(self):
+        token = create_token("barfoo", None, {"admin": True})
+        self.assertIsInstance(token, basestring)
+        token = create_token("barfoo", {}, {"admin": True})
+        self.assertIsInstance(token, basestring)
+        token = create_token("barfoo", {"foo": "bar"}, {"admin": True})
+        self.assertIsInstance(token, basestring)
+
+    def test_invalid_uid_with_admin(self):
+        with self.assertRaises(ValueError):
+            token = create_token("barfoo", {"uid": 1}, {"admin": True})
+        with self.assertRaises(ValueError):
+            token = create_token("barfoo", {"uid": None}, {"admin": True})
+        with self.assertRaises(ValueError):
+            token = create_token("barfoo", "foo", {"admin": True})
+
+if __name__ == '__main__':
+    unittest.main()
